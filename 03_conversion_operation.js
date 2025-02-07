0704e6fb92b9f990e512cd8423839f4e20d5981b
Pratik Raj <pratikraj220011@gmail.com>
2025-02-07 23:03:46 +0530

    learn conversion operation

---
+++ 03_conversion_operation.js
@@ -0,0 +1,19 @@
+let score = "33";
+let scoreInt = Number(score);
+
+console.log( typeof scoreInt);
+
+//javascript is a loosely typed language, so we can convert data types easily but it needs to be checked before use
+
+/*
+undefined = nan
+null = 0
+invalid like 2432osf = nan
+true = 1
+false = 0
+*/
+
+let islogged = 1;
+let isloggedInt = Boolean(islogged); 
+
+console.log(isloggedInt); 
