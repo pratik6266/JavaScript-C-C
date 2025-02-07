7a0e76c4b427c8ef6ea19442ded0e5c117778d36
Pratik Raj <pratikraj220011@gmail.com>
2025-02-07 19:44:24 +0530

    added datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treated all code as new version of JS
+
+// alert(3+2) // cannot use alert in node.js
+
+let id = 4;
+let name = "Pratik Raj";
+let flag = true;
+let empty = null;
+let notDefined = undefined;
+let money = 10.5;
+
+console.log(typeof(id)); //give us type of any data or variable
+console.log(typeof null);
