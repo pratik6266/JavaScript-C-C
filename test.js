8a407da6f7ea1a8c42d76e6f3e9fd79fedcb5008
Pratik Raj <pratikraj220011@gmail.com>
2025-02-07 19:24:37 +0530

    learn about varaible and its type and print

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
 console.log('Hello World!');
+
+const age = 30;
+aga = 23;
+console.log(age); //const don't let you change value and always use use let else
+let name = 'John'
+var surname = 'Doe'
+parentname = 'Mike'
+
+name = 'a';
+surname = 'b';
+parentname = 'c';
+
+console.log([name, age, surname, parentname]); // a good to print multiple values
