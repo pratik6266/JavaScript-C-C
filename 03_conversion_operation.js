bd768e90d526d43ab5e459a3641068282cfde542
Pratik Raj <pratikraj220011@gmail.com>
2025-02-07 23:53:38 +0530

    learn about operators

--- 03_conversion_operation.js
+++ 03_conversion_operation.js
@@ -1,7 +1,7 @@
-let score = "33";
-let scoreInt = Number(score);
+// let score = "33";
+// let scoreInt = Number(score);
 
-console.log( typeof scoreInt);
+// console.log( typeof scoreInt);
 
 //javascript is a loosely typed language, so we can convert data types easily but it needs to be checked before use
 
@@ -13,7 +13,30 @@ true = 1
 false = 0
 */
 
-let islogged = 1;
-let isloggedInt = Boolean(islogged); 
+// let islogged = 0;
+// let isloggedInt = Boolean(islogged); 
 
-console.log(isloggedInt); 
+// console.log(isloggedInt); 
+
+// let num = 555;
+// let numstr = String(num);
+// console.log(typeof numstr);  
+
+
+
+// ************ Operations ************
+
+
+let value = 3;
+let negValue = -value;  
+console.log(negValue);
+
+let str1 = "Hello"; 
+let str2 = " World"; 
+let str3 = str1 + str2;
+console.log(str3);  
+
+
+console.log("1" + 2); // if string first then all converted to string
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3"); // but if last then it do operation first and then convert to string
