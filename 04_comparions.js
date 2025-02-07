5425e25921a09e7cdfa7ebda86838609554573fc
Pratik Raj <pratikraj220011@gmail.com>
2025-02-08 00:21:23 +0530

    learn about heap and stack

--- 04_comparions.js
+++ 04_comparions.js
@@ -1,2 +1,31 @@
 console.log("2" == 2); // true because == convert the data type thatswhy it is inconsistent for diff data types
 console.log("2" === 2); // where as === don't changes the data type first ti checks the data types and then the data
+
+/*
+primite data types: string, number, boolean, undefined, null, symbol, bigint,  get stack,
+
+non-primitive data types: object, array, function always get heap memory
+data type are gnenerally object and function
+*/
+
+// primitive data types passes as reference so don't change the original value
+
+let obj = {
+  id : 1, 
+  name : "Rahul"
+}
+
+let obj2 = obj;
+
+obj2.name = "Rohit"; // pass by reference so it will change the original value
+
+console.log(obj.name); // Rohit
+console.log(obj2.name); // Rohit
+
+// function 
+
+function fun(){ // created a function 
+  console.log("Hello"); 
+}
+
+fun(); // calling the function  
