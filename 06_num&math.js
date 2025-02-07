f3648f754a3c12dd116290b0ad8180c2e2248605
Pratik Raj <pratikraj220011@gmail.com>
2025-02-08 01:13:20 +0530

    learn num & math

---
+++ 06_num&math.js
@@ -0,0 +1,26 @@
+// const score = 100;
+// console.log(score);
+
+// const num = new Number (100);
+// console.log(num);
+
+// console.log(num.toString().length); // convert to string and get length
+// console.log(num.toFixed(2)); // fixed till 2 decimal places
+
+// let val = 123.345;
+// console.log(val.toPrecision(3)); // round off so that it has 3 digits
+
+// const hundred = 10000000;
+// console.log(hundred.toLocaleString()); // convert to locale string and place , so that it looks good like 10,000,000
+
+
+//**************** Maths  *************** come default with JS
+// console.log(Math.PI); // give the value of PI
+// console.log(Math.min(4, 5));// for min and max also have round, abs, floor or ceil just use via Math.
+
+console.log(Math.round(Math.random() * 100)); // give random number between 0 and 1 via round and * 100 to get between 0 and 100
+
+
+
+
+
