97606cc2745909bdaadee44e504051beb462937f
Pratik Raj <pratikraj220011@gmail.com>
2025-02-10 22:00:00 +0530

    learn about conditional statement

---
+++ 14_if&else.js
@@ -0,0 +1,34 @@
+// if(true){
+//   console.log("Hello world");
+// }else console.log("Not Hello world");
+
+let islogged = true;
+
+// todo if, else if and else all are just like cpp same expect that one liner 
+
+let n = 11;
+
+// switch(n){
+//   case 1: 
+//     console.log("one");
+//     break;
+
+//   case 2:
+//     console.log("two");
+//     break;
+
+//   case 10:
+//     console.log("ten");
+//     break;
+
+//   default: //! break is important because it will excute all the code after but not default   
+//     console.log("default");
+//     break;
+// }
+
+// nullish coalescing operator (??)
+
+let num1 = undefined ?? 10; // checks and then put data if not get data put null else put data that come and work with null and undefined
+console.log(num1);
+
+num1 === 10 ? console.log("10") : console.log("not"); // oneliner if and else
